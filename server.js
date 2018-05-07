09ff2d0884910d37f3af792bab86fddc7860899c
erdem koral <koral.erdem@gmail.com>
2018-05-07 12:21:00 -0700

    change port back to 3001

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ connect();
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 server.listen(port, () => {
     console.log('server is running on', server.address().port);
