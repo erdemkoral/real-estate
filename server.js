44714ec09e112558aa32eaf2897079acd1afad39
erdem koral <koral.erdem@gmail.com>
2018-05-09 10:49:08 -0700

    add geocode services

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const http = require('http');
 const app = require('./lib/app');
 const connect = require('./lib/connect');
 connect();
-// const mongoose = require('mongoose');
-// const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/realestate';
-// mongoose.connect(DB_URI);
 
 const server = http.createServer(app);
 
